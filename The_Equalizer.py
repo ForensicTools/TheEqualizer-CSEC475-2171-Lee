d6e0bfc5244ed892170a74486b0f42d48bab2743
Wesley <wtl5736@cs.rit.edu>
2017-11-23 16:52:11 -0500

    16th Commit
    OS Type selection

--- The_Equalizer.py
+++ The_Equalizer.py
@@ -2,4 +2,19 @@
 
 import os
 
-os.system("python Program\ Files/The_Equalizer.py")
+print """
+What OS are you running?
+
+1) Windows
+2) MacOS
+3) Linux
+"""
+
+os = raw_input("OS Type: ")
+
+if os == '1':
+	os.system("python \"Program Files\The_Equalizer.py\"")
+elif os == '2':
+	os.system("python Program\ Files/The_Equalizer.py")
+elif os == '3':
+	os.system("python Program\ Files/The_Equalizer.py")
