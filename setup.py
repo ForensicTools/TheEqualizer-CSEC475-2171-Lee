af3e99c38aa9c3cdbf56c7edd17f3a956c3cff19
Wesley <wtl5736@cs.rit.edu>
2017-11-25 21:42:15 -0500

    28th Commit

--- setup.py
+++ setup.py
@@ -12,13 +12,55 @@ Description:
 #!/usr/bin/python
 import os, sys
 
+print """What OS are you running?
+
+1) MacOS
+2) Linux
+"""
+
+def os_choice():
+	print """What OS are you running?
+
+	1) MacOS
+	2) Linux
+	"""
+	os_choice = raw_input("OS Type: ")
+	
+	if os_choice == '1':
+		install_pymods_macos()
+	elif os_choice == '2':
+		install_pymods_linux()
+
 # Installs required python modules
-def install_pymods():
+def install_pymods_macos():
 	# Install Python Module
 	os.system("clear")
 	print "Installing Necessary Python Modules...\n\n"
 	os.system("pip install -r requirements.txt")
 
+def install_pymods_linux():
+	# Install Python Module
+	os.system("clear")
+	print "Installing pip for Python...\n"
+	os.system("apt-get install python-pip -y")
+	os.system("pip install --upgrade pip")
+	
+	print "\nInstalling Necessary Python Modules...\n\n"
+	os.system("pip install -r requirements.txt")
+
+def is_root():
+	notRoot = False
+	try:
+		# check whether user is root
+		if (os.geteuid() != 0):
+			print("\n{0}ERROR: Network Vulnerability Programs v2.0 must be run with root privileges. Try again with sudo:\n\t{1}$ sudo python Network_Vulnerability_Program_v2.0.py{2}\n").format(RED, GREEN, END)
+			notRoot = True
+	except:
+		# then user is probably on windows
+		pass
+		
+	if (notRoot):
+		raise SystemExit
 
 # Install Plotly Account
 def install_plotly(username, api_key):
@@ -45,7 +87,8 @@ def run_program(user_input):
 
 # Main to run all everything
 def main():
-	install_pymods()
+	is_root()
+	os_choice()
 	
 	print "Installing Plotly Account...\n\n"
 	username = raw_input("\nEnter Plotly Username: ")
